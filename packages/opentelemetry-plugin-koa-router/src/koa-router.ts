ac059ae68d7f6363844fa2b00eed0d2352ccae2f
Andrii Hrachov <andrey.grachov@depositphotos.com>
2020-03-12 23:18:50 +0200

    make some progress

---
+++ packages/opentelemetry-plugin-koa-router/src/koa-router.ts
@@ -0,0 +1,90 @@
+/*!
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { BasePlugin } from '@opentelemetry/core';
+import * as koaRouter from '@koa/router';
+import * as shimmer from 'shimmer';
+
+import {
+  KoaRouterPluginConfig,
+  PathParams,
+  KoaRouter,
+  KoaLayer,
+} from './types';
+import { VERSION } from './version';
+/**
+ * This symbol is used to mark koa-router layer as being already instrumented
+ */
+export const kLayerPatched: unique symbol = Symbol('koa-router-layer-patched');
+
+/** Koa-router instrumentation plugin for OpenTelemetry */
+export class KoaRouterPlugin extends BasePlugin<typeof koaRouter> {
+  static readonly component = 'koa-router';
+  protected _config!: KoaRouterPluginConfig;
+
+  constructor(readonly moduleName: string) {
+    super('@opentelemetry/plugin-koa-router', VERSION);
+  }
+  /**
+   * Patches Koa-router
+   */
+  protected patch() {
+    this._logger.debug('Patching Koa router');
+
+    if (this._moduleExports === undefined || this._moduleExports === null) {
+      return this._moduleExports;
+    }
+
+    const routerProto = (this._moduleExports as unknown) as koaRouter;
+    shimmer.wrap(routerProto, 'register', this._getRegisterPatch.bind(this));
+
+    return this._moduleExports;
+  }
+
+  /** Unpatches all Koa router patched functions. */
+  unpatch(): void {
+    const routerProto = (this._moduleExports as unknown) as koaRouter;
+    shimmer.unwrap(routerProto, 'use');
+  }
+
+  /**
+   * Get the patch for Router.register function
+   * @param original
+   */
+  private _getRegisterPatch(
+    original: (path: PathParams) => koaRouter.IMiddleware
+  ) {
+    const plugin = this;
+    return function route_trace(
+      this: KoaRouter,
+      ...args: Parameters<typeof original>
+    ) {
+      const route = original.apply(this, args);
+      const layer = this.stack[this.stack.length - 1] as KoaLayer;
+      plugin._applyPatch(
+        layer,
+        typeof args[0] === 'string' ? args[0] : undefined
+      );
+      return route;
+    };
+  }
+
+  _applyPatch(layer: KoaLayer, layerPath?: string) {}
+
+  _getRouter() {}
+}
+
+export const plugin = new KoaRouterPlugin(KoaRouterPlugin.component);
---
+++ packages/opentelemetry-plugin-koa-router/src/types.ts
@@ -0,0 +1,35 @@
+export type KoaRouterPluginConfig = {
+  name: string;
+};
+
+export type PathParams = string | RegExp | Array<string | RegExp>;
+
+// https://github.com/koajs/router/blob/master/lib/router.js#L65
+export type KoaRouter = {
+  params: { [key: string]: string };
+  opts: KoaOptions; // @TODO
+  methods: string[];
+  stack: KoaLayer[];
+};
+
+export type KoaBaseOptions = {
+  name: string;
+  strict: boolean;
+  prefix: string;
+};
+
+export type KoaOptions = {
+  methods: String[];
+} & KoaBaseOptions;
+
+// https://github.com/koajs/router/blob/master/lib/layer.js#L21
+export type KoaLayer = {
+  ignoreCaptures: boolean;
+  name: string;
+  end: boolean;
+  prefix: string;
+  paramNames: string[];
+  path: string;
+  regexp: RegExp;
+  stack: Function | Function[];
+};
